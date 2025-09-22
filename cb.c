561b99198822cae24d427d8fe1a6d50d14b50750
onraj55555 <jarnovanruymbeke@gmail.com>
2025-09-22 09:43:46 +0200

    testing code

--- cb.c
+++ cb.c
@@ -1,7 +1,12 @@
+#include <stdio.h>
 #define CB_IMPLEMENTATION
 #include "cb.h"
 
 int main(int argc, char ** argv) {
     parse_arguments(argc, argv);
+    char * argument = get_argument_from_flag("-o");
+    printf("Arg: %s\n", argument);
+    printf("Has flag -g: %d\n", has_flag("-g"));
+    printf("Argument at index 1 is run: %d\n", has_argument_at_intex("run", 1));
     return 0;
 }
