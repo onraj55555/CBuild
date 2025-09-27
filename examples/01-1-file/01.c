80c4a0932af8c2ec9e9f09586fb1161d0fd77f3b
onraj55555 <jarnovanruymbeke@gmail.com>
2025-09-27 19:45:44 +0200

    init: added code fragments to show of feature use

---
+++ examples/01-1-file/01.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("01 DONE!\n");
+    return 0;
+}
---
+++ examples/01-1-file/cb.c
@@ -0,0 +1,9 @@
+#define CB_IMPLEMENTATION
+#include "../../cb.h"
+
+int main(int argc, char ** argv) {
+    command_t * cmd = command_init("/usr/sbin/cc");
+    command_append_n(cmd, "01.c", "-o", "01", NULL);
+    command_execute(cmd);
+    return 0;
+}
