7d76e247985a3b312a0c7e30a369bc123ece74bd
onraj55555 <jarnovanruymbeke@gmail.com>
2025-09-28 21:50:10 +0200

    update: freed command instance

--- examples/01-1-file/cb.c
+++ examples/01-1-file/cb.c
@@ -5,5 +5,6 @@ int main(int argc, char ** argv) {
     command_t * cmd = command_init("/usr/sbin/cc");
     command_append_n(cmd, "01.c", "-o", "01", NULL);
     command_execute(cmd);
+    command_deinit(cmd);
     return 0;
 }
